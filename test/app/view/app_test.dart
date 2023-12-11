ec263afe8374453395711e7f7e2c05d788414785
carolgsomnio <carglassm@gmail.com>
2023-12-11 16:39:27 -0300

    tests and flutter analyze

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router_internal/app/app.dart';
-import 'package:go_router_internal/counter/counter.dart';
+import 'package:go_router_internal/home/home.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders home page', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
